44624e6a81e0c24f2e2adb67aecb622019a9ccc4
Philipp Smirnov <philip.smirnov@gotlilabs.com>
2018-08-07 18:02:27 +0300

    wip

---
+++ euler5.py
@@ -0,0 +1,7 @@
+i=20;
+answer=0;
+while answer == 0:
+    i=i+1;
+    if(i%20==0 and i%19==0 and i%18==0 and i%17==0 and i%16==0 and i%15==0 and i%14==0 and i%13==0 and i%12==0 and i%11==0 and i%9==0 and i%8==0 and i%7==0 and i%6==0):
+            answer=i;
+print(answer);
